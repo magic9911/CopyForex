cea4a5a4c3f2efba21adfcbc57e3888b1517dd10
Teetat Kitwacharakul <black_magic_9911@hotmail.com>
2015-11-04 09:48:34 +0700

    connect

---
+++ testUnmanagedDLL/SendData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+
+namespace CopyForex
+{
+    class SendData
+    {
+        System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
+        NetworkStream serverStream;
+
+        public void Connect()
+        {
+            clientSocket.Connect("127.0.0.1", 9000);
+        }
+
+        public string Send(string data)
+        {
+            NetworkStream serverStream = clientSocket.GetStream();
+            byte[] outStream = System.Text.Encoding.ASCII.GetBytes(data);
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+
+            byte[] inStream = new byte[10025];
+            serverStream.Read(inStream, 0, (int)clientSocket.ReceiveBufferSize);
+            string returndata = System.Text.Encoding.ASCII.GetString(inStream);
+            return returndata;
+        }
+    }
+}
--- testUnmanagedDLL/UnmanagedExports.cs
+++ testUnmanagedDLL/UnmanagedExports.cs
@@ -5,12 +5,14 @@ using System.Windows.Forms;
 using testUMD;
 using System.Threading;
 using System;
+using CopyForex;
 
 namespace testUnmanagedDLL {
     class CopyDll {
 
         private static Form1 oneForm;
         private static Thread appThread;
+        private static SendData senddata;
 
         public static string res = string.Empty;
 
@@ -45,49 +47,57 @@ namespace testUnmanagedDLL {
         [DllExport("Data_POST", CallingConvention = CallingConvention.StdCall)]
         public static IntPtr Data_POST(double Balance, IntPtr charPos)
         {
-            string res = string.Empty;
-            if (null == oneForm)
-                return Marshal.StringToHGlobalUni(res);
+            //string res = string.Empty;
+            //if (null == oneForm)
+            //    return Marshal.StringToHGlobalUni(res);
 
             var Pos = Marshal.PtrToStringAnsi(charPos);
 
-            oneForm.Invoke(new Action(() => {
-                res = oneForm.SendData("POST;" + Balance.ToString() + ";" + Pos);
-            }));
+            //oneForm.Invoke(new Action(() => {
+            //    res = oneForm.SendData("POST;" + Balance.ToString() + ";" + Pos);
+            //}));
 
+            senddata.Send("POST;" + Balance.ToString() + ";" + Pos);
             return Marshal.StringToHGlobalUni(res);
         }
 
         [DllExport("Get_NewOrder", CallingConvention = CallingConvention.StdCall)]
         public static IntPtr Get_NewOrder(IntPtr symbol)
         {
-            string res = string.Empty;
-            if (null == oneForm)
-                return Marshal.StringToHGlobalUni(res);
+            //string res = string.Empty;
+            //if (null == oneForm)
+            //    return Marshal.StringToHGlobalUni(res);
 
             var Symbol = Marshal.PtrToStringAnsi(symbol);
 
-            oneForm.Invoke(new Action(() => {
-                res = oneForm.SendData("NewOrder;" + Symbol);
-            }));
+            //oneForm.Invoke(new Action(() => {
+            //    res = oneForm.SendData("NewOrder;" + Symbol);
+            //}));
 
+            senddata.Send("NewOrder;" + Symbol);
             return Marshal.StringToHGlobalUni(res);
         }
 
         [DllExport("Get_CloseOrder", CallingConvention = CallingConvention.StdCall)]
         public static IntPtr Get_CloseOrder(int id)
         {
-            string res = string.Empty;
-            if (null == oneForm)
-                return Marshal.StringToHGlobalUni(res);
+            //string res = string.Empty;
+            //if (null == oneForm)
+            //    return Marshal.StringToHGlobalUni(res);
 
-            oneForm.Invoke(new Action(() => {
-                res = oneForm.SendData("CloseOrder;" + id.ToString());
-            }));
+            //oneForm.Invoke(new Action(() => {
+            //    res = oneForm.SendData("CloseOrder;" + id.ToString());
+            //}));
 
+            senddata.Send("CloseOrder;" + id.ToString());
             return Marshal.StringToHGlobalUni(res);
         }
 
+        [DllExport("Connect", CallingConvention = CallingConvention.StdCall)]
+        public static void Connect()
+        {
+            senddata.Connect();
+        }
 
         [DllExport("FormChangeTitle", CallingConvention = CallingConvention.StdCall)]
         public static void FormChangeTitle(IntPtr title)
